2e244fe7e269f80e9ecb09cd070a8e1bbf9ebc62
bianco-e <bianco.emiliozzi@gmail.com>
2021-04-21 17:18:41 -0300

    Add Campaign route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Campaign from "./pages/Campaign";
 import Collection from "./pages/Collection";
 import Game from "./pages/Game";
 import Menu from "./pages/Menu";
@@ -12,6 +13,7 @@ const MainRouter = () => {
       <Route exact path="/" render={() => <WelcomePage />} />
       <Route exact path="/play" render={() => <Game />} />
       <Route exact path="/menu" render={() => <Menu />} />
+      <Route exact path="/campaign" render={() => <Campaign />} />
       <Route exact path="/profile" render={() => <Profile />} />
       <Route exact path="/collection" render={() => <Collection />} />
     </BrowserRouter>
---
+++ src/pages/Campaign.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import MenuLayout from "../components/MenuLayout";
+
+export default function Campaign() {
+  return (
+    <MenuLayout>
+      <h2>Campaign</h2>
+    </MenuLayout>
+  );
+}
+
+const Wrapper = styled.div``;
