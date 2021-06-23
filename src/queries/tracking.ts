53c170963152398675d8fc339a66ccf9b78b0ae8
bianco-e <bianco.emiliozzi@gmail.com>
2021-06-23 09:13:37 -0300

    Log isDevelopment boolean

--- src/queries/tracking.ts
+++ src/queries/tracking.ts
@@ -2,6 +2,9 @@ import { API_BASE_URL } from "../utils/constants";
 import { ACPost } from "./user";
 const isDevelopment = process.env.REACT_APP_ENVIRONMENT === "dev";
 
+console.log(process.env.REACT_APP_ENVIRONMENT);
+console.log(isDevelopment);
+
 interface Visit {
   auth_id?: string;
   utm?: string;
@@ -11,6 +14,7 @@ interface Visit {
 
 export const trackAction = (visit: Visit) => {
   if (isDevelopment) return;
+  console.log("inside", isDevelopment);
   return fetch(`${API_BASE_URL}track_action`, {
     ...ACPost(visit),
   })
